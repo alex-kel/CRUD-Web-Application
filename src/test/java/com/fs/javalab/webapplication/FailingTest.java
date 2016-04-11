89909b6d8b0350ee5185cb5fb3fe6f195ee7002f
Alexander Kel <1906.kas@gmail.com>
2016-04-11 09:30:57 +0300

    Fail test commit

---
+++ src/test/java/com/fs/javalab/webapplication/FailingTest.java
@@ -0,0 +1,17 @@
+package com.fs.javalab.webapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Alex on 11.04.16.
+ */
+
+public class FailingTest {
+
+    @Test
+    public void fails() {
+        fail();
+    }
+}
